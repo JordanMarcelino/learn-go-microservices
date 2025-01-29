7ef1151536b4df63f1cd5c389024099b67e972d1
Jordan Marcelino <jhordan.marcelino@gmail.com>
2025-01-29 23:46:24 +0700

    feat(auth): add db migrations

---
+++ auth-service/db/migrations/000002_create_table_verification.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS user_verifications (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    token VARCHAR(255) NOT NULL
+);
+CREATE INDEX IF NOT EXISTS idx_fk_user_verification_user_id ON user_verifications (user_id);
