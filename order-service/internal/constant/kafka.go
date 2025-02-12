27ebfe8e6f8340090af72c32d6dafe8e48f6491a
Jordan Marcelino <jhordan.marcelino@gmail.com>
2025-02-12 14:39:59 +0700

    feat(order): add kafka constant

---
+++ order-service/internal/constant/kafka.go
@@ -0,0 +1,28 @@
+package constant
+
+const (
+	ProductCreatedTopic = "product-created"
+	ProductUpdatedTopic = "product-updated"
+	ProductDeletedTopic = "product-deleted"
+
+	OrderCreatedTopic                  = "order-created"
+	OrderCreatedTopicNumPartitions     = 3
+	OrderCreatedTopicReplicationFactor = 1
+
+	OrderCancelledTopic                  = "order-cancelled"
+	OrderCancelledTopicNumPartitions     = 3
+	OrderCancelledTopicReplicationFactor = 1
+)
+
+const (
+	ProductCreatedConsumerGroup = "product-created-consumer-group"
+
+	ProductUpdatedConsumerGroup = "product-updated-consumer-group"
+
+	ProductDeletedConsumerGroup = "product-deleted-consumer-group"
+)
+
+const (
+	KafkaRetryDelay = 3
+	KafkaRetryLimit = 3
+)
