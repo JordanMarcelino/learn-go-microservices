68a8af0c13afe3a9d7d8730e3c566445b4101db2
Jordan Marcelino <jhordan.marcelino@gmail.com>
2025-02-14 22:24:21 +0700

    feat(order): add create order

---
+++ order-service/internal/dto/order.go
@@ -0,0 +1,66 @@
+package dto
+
+import (
+	"github.com/jordanmarcelino/learn-go-microservices/order-service/internal/entity"
+	"github.com/shopspring/decimal"
+)
+
+type OrderResponse struct {
+	ID          int64                `json:"id"`
+	TotalAmount decimal.Decimal      `json:"total_amount"`
+	Description string               `json:"description"`
+	Status      string               `json:"status"`
+	Items       []*OrderItemResponse `json:"items"`
+	CreatedAt   string               `json:"created_at"`
+	UpdatedAt   string               `json:"updated_at"`
+}
+
+type OrderItemResponse struct {
+	ID        int64           `json:"id"`
+	ProductID int64           `json:"product_id"`
+	Quantity  int             `json:"quantity"`
+	Price     decimal.Decimal `json:"price"`
+}
+
+type CreateOrderRequest struct {
+	CustomerID  int64
+	RequestID   string              `json:"request_id" binding:"required"`
+	Description string              `json:"description"`
+	Items       []*OrderItemRequest `json:"items" binding:"required,min=1,dive"`
+}
+
+type OrderItemRequest struct {
+	ProductID int64 `json:"product_id" binding:"required"`
+	Quantity  int   `json:"quantity" binding:"required,min=1"`
+}
+
+func ToOrderResponses(orders []*entity.Order) []*OrderResponse {
+	res := []*OrderResponse{}
+	for _, order := range orders {
+		res = append(res, ToOrderResponse(order))
+	}
+	return res
+
+}
+
+func ToOrderResponse(order *entity.Order) *OrderResponse {
+	items := []*OrderItemResponse{}
+	for _, item := range order.Items {
+		items = append(items, &OrderItemResponse{
+			ID:        item.ID,
+			ProductID: item.ProductID,
+			Quantity:  item.Quantity,
+			Price:     item.Price,
+		})
+	}
+
+	return &OrderResponse{
+		ID:          order.ID,
+		TotalAmount: order.TotalAmount,
+		Description: order.Description,
+		Status:      order.Status,
+		Items:       items,
+		CreatedAt:   order.CreatedAt.String(),
+		UpdatedAt:   order.UpdatedAt.String(),
+	}
+}
---
+++ order-service/internal/usecase/order_usecase.go
@@ -0,0 +1,110 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/bsm/redislock"
+	"github.com/jordanmarcelino/learn-go-microservices/order-service/internal/constant"
+	. "github.com/jordanmarcelino/learn-go-microservices/order-service/internal/dto"
+	"github.com/jordanmarcelino/learn-go-microservices/order-service/internal/entity"
+	"github.com/jordanmarcelino/learn-go-microservices/order-service/internal/httperror"
+	"github.com/jordanmarcelino/learn-go-microservices/order-service/internal/repository"
+	"github.com/jordanmarcelino/learn-go-microservices/order-service/internal/utils/redisutils"
+	"github.com/shopspring/decimal"
+)
+
+type OrderUseCase interface {
+	Save(ctx context.Context, req *CreateOrderRequest) (*OrderResponse, error)
+}
+
+type orderUseCaseImpl struct {
+	DataStore      repository.DataStore
+	LockRepository repository.LockRepository
+}
+
+func NewOrderUseCase(
+	dataStore repository.DataStore,
+	lockRepository repository.LockRepository,
+) OrderUseCase {
+	return &orderUseCaseImpl{
+		DataStore:      dataStore,
+		LockRepository: lockRepository,
+	}
+}
+
+func (u *orderUseCaseImpl) Save(ctx context.Context, req *CreateOrderRequest) (*OrderResponse, error) {
+	lockKey := redisutils.NewLockKey(req.RequestID, req.CustomerID)
+	ttl := constant.CreateOrderTTL
+	opt := &redislock.Options{
+		RetryStrategy: redislock.LimitRetry(redislock.LinearBackoff(constant.CreateOrderRetryInterval), constant.CreateOrderRetryLimit),
+	}
+
+	lock, err := u.LockRepository.Get(ctx, lockKey, ttl, opt)
+	if err != nil {
+		return nil, err
+	}
+	defer lock.Release(ctx)
+
+	res := new(OrderResponse)
+	err = u.DataStore.Atomic(ctx, func(ds repository.DataStore) error {
+		orderRepository := ds.OrderRepository()
+		productRepository := ds.ProductRepository()
+
+		order, err := orderRepository.FindByRequestID(ctx, req.RequestID)
+		if err != nil {
+			return err
+		}
+		if order != nil && order.CustomerID == req.CustomerID {
+			res = ToOrderResponse(order)
+			return nil
+		}
+
+		order = &entity.Order{
+			RequestID:   req.RequestID,
+			CustomerID:  req.CustomerID,
+			Description: req.Description,
+			Status:      constant.ORDER_PENDING,
+			Items:       []*entity.OrderItem{},
+		}
+		productIds := []int64{}
+		for _, item := range req.Items {
+			productIds = append(productIds, item.ProductID)
+		}
+
+		products, err := productRepository.FindAllByIDForUpdate(ctx, productIds)
+		if err != nil {
+			return err
+		}
+		if len(products) != len(productIds) {
+			return httperror.NewProductNotFoundError()
+		}
+
+		totalAmount := decimal.Decimal{}
+		for i, product := range products {
+			if product.Quantity < req.Items[i].Quantity {
+				return httperror.NewInsufficientProductStockError()
+			}
+			product.Quantity -= req.Items[i].Quantity
+
+			totalAmount = totalAmount.Add(product.Price.Mul(decimal.NewFromInt(int64(req.Items[i].Quantity))))
+			order.Items = append(order.Items, &entity.OrderItem{ProductID: product.ID, Price: product.Price, Quantity: req.Items[i].Quantity})
+		}
+		order.TotalAmount = totalAmount
+
+		if err := productRepository.UpdateAllQuantity(ctx, products); err != nil {
+			return err
+		}
+		if err := orderRepository.Save(ctx, order); err != nil {
+			return err
+		}
+
+		res = ToOrderResponse(order)
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
